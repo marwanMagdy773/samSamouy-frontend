ee60350ba1a3228635f3b370e8dae40d50412a40
marwanMagdy773 <web.dev@samsamouy.com>
2025-07-23 16:49:33 +0300

    update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './styles.css';
 import ReactGA from 'react-ga4';
 
 // ✅ حط الـ Measurement ID بتاعك هنا
-ReactGA.initialize('G-4V9BJRLVHL');
+ReactGA.initialize('G-YDZRCPZWT8');
 
 // ✅ تسجل أول زيارة للموقع (Page View)
 ReactGA.send({ hitType: "pageview", page: window.location.pathname });
