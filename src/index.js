1db8ca67b9a7336e1942ccbe224a4988bcecdd13
marwanMagdy773 <web.dev@samsamouy.com>
2025-07-23 16:41:19 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import ReactGA from 'react-ga4';
 
+// ✅ حط الـ Measurement ID بتاعك هنا
+ReactGA.initialize('G-4V9BJRLVHL');
+
+// ✅ تسجل أول زيارة للموقع (Page View)
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
