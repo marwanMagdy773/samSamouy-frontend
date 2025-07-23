5464257f21b07c2bdf155195fd0aeb8c99547b08
marwanMagdy773 <web.dev@samsamouy.com>
2025-07-23 18:04:44 +0300

    update

--- src/analytics.js
+++ src/analytics.js
@@ -2,7 +2,7 @@
 import ReactGA from 'react-ga4';
 
 export const initGA = () => {
-  ReactGA.initialize("G-XXXXXXXXXX"); // استبدل بـ Measurement ID بتاعك
+  ReactGA.initialize("G-YDZRCPZWT8"); // استبدل بـ Measurement ID بتاعك
   ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
 };
 
